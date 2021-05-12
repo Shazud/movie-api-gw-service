8f955912203d8e331688ae80df3a22a3c99d86cb
Shazud <david.pava@protonmail.com>
2021-05-12 14:58:21 +0300

    Disabled HttpsRedirection

--- Startup.cs
+++ Startup.cs
@@ -47,7 +47,7 @@ namespace MovieAPIGatewayService
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MovieAPIGatewayService v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
